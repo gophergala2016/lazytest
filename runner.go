89550a8be3f3d9dec046e522aa3596b1d1d53840
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2016-01-24 18:10:21 -0500

    runner

--- runner.go
+++ runner.go
@@ -1,31 +1,105 @@
 package lazytest
 
-type TestResult uint8
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type testQueue struct {
+	tests []Batch
+}
 
 const (
-	TestFailed TestResult = iota
-	TestPassed
-	TestErrored
+	RunnerIdle int32 = iota
+	RunnerBusy
+)
+
+type TestStatus int8
+
+const (
+	StatusPending TestStatus = iota
+	StatusSkipped
+	StatusFailed
+	StatusPanicked
+	StatusPassed
 )
 
 type TestReport struct {
 	Name    string
-	Result  TestResult
+	Package string
+	Status  TestStatus
 	Message string
 }
 
-type Report []TestReport
+var (
+	runnerDone   chan struct{}
+	runnerStatus int32
+	mux          sync.Mutex
+	queue        *testQueue = &testQueue{}
+	rep          chan Report
+)
 
-var rep chan Report
+type Report []TestReport
 
 func Runner(batch chan Batch) chan Report {
 	rep = make(chan Report, 50)
-
-	go testRunner(batch)
-
+	go queueTests(batch, rep)
 	return rep
 }
 
-func testRunner(batch chan Batch) {
+func (t *testQueue) run() {
+	packageTests := make(map[string][]string)
+	for _, test := range t.tests {
+		packageTests[test.Package] = append(packageTests[test.Package], regexp.QuoteMeta(test.TestName))
+	}
+	for pkg, tests := range packageTests {
+		testRegexp := fmt.Sprintf("'(%s)'", strings.Join(tests, "|"))
 
+		cmd := exec.Command("go", "test", pkg, "-run", testRegexp)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log(err.Error())
+		}
+		log(string(out))
+	}
+	atomic.StoreInt32(&runnerStatus, RunnerIdle)
+	runnerDone <- struct{}{}
+}
+
+func queueTests(batch chan Batch, rep chan Report) {
+	var delay *time.Timer
+	for {
+		select {
+		case b := <-batch:
+			mux.Lock()
+			if delay == nil {
+				delay = time.NewTimer(time.Second * 2)
+			}
+			queue.tests = append(queue.tests, b)
+			mux.Unlock()
+
+		case <-delay.C:
+			mux.Lock()
+			if atomic.CompareAndSwapInt32(&runnerStatus, RunnerIdle, RunnerBusy) {
+				delay = nil
+				go queue.run()
+				queue = &testQueue{}
+			}
+			mux.Unlock()
+
+		case <-runnerDone:
+			mux.Lock()
+			if delay == nil && len(queue.tests) > 0 {
+				atomic.StoreInt32(&runnerStatus, RunnerBusy)
+				go queue.run()
+				queue = &testQueue{}
+			}
+			mux.Unlock()
+		}
+	}
 }
