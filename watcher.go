269e58a79d90f7d6358e4e83afc5a6b4273e7e8c
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-01-23 19:18:18 -0500

    Removed global variables from watcher.go

--- watcher.go
+++ watcher.go
@@ -9,18 +9,6 @@ import (
 	"gopkg.in/fsnotify.v1"
 )
 
-var (
-	watcher    *fsnotify.Watcher
-	events     chan Mod
-	root       string
-	extensions []string
-	exclude    []string
-
-	/*fileWatches   = []*fsnotify.Watcher{}
-	folderWatches = []*fsnotify.Watcher{}
-	lock          sync.Mutex*/
-)
-
 type Mod struct {
 	Package  string
 	FilePath string
@@ -28,50 +16,65 @@ type Mod struct {
 	Line     int
 }
 
-func Watch(r string, ext []string, excl []string) (chan Mod, error) {
-	var err error
-	if watcher, err = fsnotify.NewWatcher(); err != nil {
-		return nil, err
+type fileWalker struct {
+	extensions []string
+	exclude    []string
+	watcher    *fsnotify.Watcher
+}
+
+func (w *fileWalker) walkFunction(path string, info os.FileInfo,
+	err error) error {
+
+	if info.IsDir() {
+		if !w.isIncluded(path) {
+			return filepath.SkipDir
+		}
+		if len(path) > 1 && strings.HasPrefix(filepath.Base(path), ".") {
+			return filepath.SkipDir
+		}
+		if err := w.watcher.Add(path); err != nil {
+			panic(err) // TODO: better error handling
+		}
+		return err
 	}
-	root = r
-	exclude = excl
-	extensions = ext
 
-	go handleEvents()
+	if w.isIncluded(path) {
+		if err := w.watcher.Add(path); err != nil {
+			panic(err) // TODO: better error handling
+		}
+	}
 
-	// get absolute path
-	absP, err := filepath.Abs(r)
+	return err
+}
+
+func Watch(root string, extensions []string, exclude []string) (chan Mod,
+	error) {
+
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return nil, err
 	}
 
-	// walk folder tree starting at the path
-	filepath.Walk(absP, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			if !isIncluded(path) {
-				return filepath.SkipDir
-			}
-			if len(path) > 1 && strings.HasPrefix(filepath.Base(path), ".") {
-				return filepath.SkipDir
-			}
-			if err := watcher.Add(path); err != nil {
-				panic(err) // this needs better error handling
-			}
-		} else if isIncluded(path) {
-			if err := watcher.Add(path); err != nil {
-				panic(err) // this needs better error handling
-			}
-		}
-		return err
-	})
+	absolutePath, err := filepath.Abs(root)
+	if err != nil {
+		return nil, err
+	}
 
-	return events, nil
+	walker := &fileWalker{
+		extensions: extensions,
+		exclude:    exclude,
+		watcher:    watcher,
+	}
+
+	events := make(chan Mod, 50)
+	go walker.handleEvents(events)
+	return events, filepath.Walk(absolutePath, walker.walkFunction)
 }
 
-func handleEvents() {
+func (w *fileWalker) handleEvents(events chan Mod) {
 	for {
 		select {
-		case e := <-watcher.Events:
+		case e := <-w.watcher.Events:
 			// don't trigger events on file being renamed or chmod changed
 			if (e.Op|fsnotify.Write == e.Op) || (e.Op|fsnotify.Create == e.Op) || (e.Op|fsnotify.Remove == e.Op) {
 				events <- Mod{
@@ -80,28 +83,28 @@ func handleEvents() {
 			}
 			// TODO: remove old watches on delete, add new watches on create, do both on rename
 
-		case err := <-watcher.Errors:
+		case err := <-w.watcher.Errors:
 			log(fmt.Sprintf("Watcher error %v", err))
 		}
 	}
 }
 
-func isIncluded(path string) bool {
+func (w *fileWalker) isIncluded(path string) bool {
 	include := false
 
 	// if no extensions were provided match all
-	if len(extensions) == 0 {
+	if len(w.extensions) == 0 {
 		include = true
 	} else {
 		ext := filepath.Ext(path)
-		for _, e := range extensions {
+		for _, e := range w.extensions {
 			if ext == e {
 				include = true
 			}
 		}
 	}
 
-	for _, e := range exclude {
+	for _, e := range w.exclude {
 		if filepath.HasPrefix(path, e) {
 			include = false
 		}
